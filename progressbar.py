cc78f77f71ec165fbf2f48dfc970b1a0f4ec83ed
Fan Ni <ni.fan@outlook.com>
2017-01-10 22:11:17 +0800

    update

--- progressbar.py
+++ progressbar.py
@@ -6,29 +6,44 @@ class ProgressBar(object):
 	def __init__(self, total = 100, current = 0):
 		self.total = total
 		self.current = current
-		self.percentage = current * 100 // total
-		self.length = percentage // 2
-		self.content = 'Progress:  [' + '#' * self.length + '.' * (50 - self.length) + ']  ' + str(self.percentage) + '%'
+		self.percentage = self.current * 100 // self.total
+		self.length = self.percentage // 2
+		self.content = 'Progress:  [' + '#' * self.length + '.' * (50 - self.length) + ']  ' + str(self.percentage) + '%' +'\r'
 
 	def show(self):
-		sys.stdout.write(' ' * len(self.content))
+		sys.stdout.write(' ' * len(self.content) + '\r')
 		sys.stdout.flush()
 		sys.stdout.write(self.content)
 		sys.stdout.flush()
-		time.sleep(1)
+		time.sleep(0.2)
 
 	def present(self):
 		print(self.content)
 
-	def modify(self, newcurrent = self.current, newtotal = self.total):
-		pass
+	def modify(self, newcurrent, newtotal):
+		self.total = newtotal
+		self.current = newcurrent
+		self.percentage = self.current * 100 // self.total
+		self.length = self.percentage // 2
+		self.content = 'Progress:  [' + '#' * self.length + '.' * (50 - self.length) + ']  ' + str(self.percentage) + '%' + '\r'
 
-	def update():
-		pass
+	def update(self, newcurrent):
+		self.current = newcurrent
+		self.percentage = self.current * 100 // self.total
+		self.length = self.percentage // 2
+		self.content = 'Progress:  [' + '#' * self.length + '.' * (50 - self.length) + ']  ' + str(self.percentage) + '%' + '\r'
+		self.show()
 
-	def increase():
-		pass
+	def increase(self, increment = 1):
+		self.current = self.current + increment
+		self.percentage = self.current * 100 // self.total
+		self.length = self.percentage // 2
+		self.content = 'Progress:  [' + '#' * self.length + '.' * (50 - self.length) + ']  ' + str(self.percentage) + '%' + '\r'
+		self.show()
 
-	def restart():
-		pass
-		
+	def restart(self):
+		self.current = 0
+		self.percentage = self.current * 100 // self.total
+		self.length = self.percentage // 2
+		self.content = 'Progress:  [' + '#' * self.length + '.' * (50 - self.length) + ']  ' + str(self.percentage) + '%' + '\r'
+
