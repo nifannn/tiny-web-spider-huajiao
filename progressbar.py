8ba372cb9f99b7d83ba4c7b0dba8596a3f79aa6e
Fan Ni <ni.fan@outlook.com>
2017-01-10 20:29:30 +0800

    update

--- progressbar.py
+++ progressbar.py
@@ -2,8 +2,33 @@ import sys
 import time
 
 class ProgressBar(object):
-	"""docstring for ProgressBar"""
-	def __init__(self, arg):
-		super(ProgressBar, self).__init__()
-		self.arg = arg
+	"""ProgressBar"""
+	def __init__(self, total = 100, current = 0):
+		self.total = total
+		self.current = current
+		self.percentage = current * 100 // total
+		self.length = percentage // 2
+		self.content = 'Progress:  [' + '#' * self.length + '.' * (50 - self.length) + ']  ' + str(self.percentage) + '%'
+
+	def show(self):
+		sys.stdout.write(' ' * len(self.content))
+		sys.stdout.flush()
+		sys.stdout.write(self.content)
+		sys.stdout.flush()
+		time.sleep(1)
+
+	def present(self):
+		print(self.content)
+
+	def modify(self, newcurrent = self.current, newtotal = self.total):
+		pass
+
+	def update():
+		pass
+
+	def increase():
+		pass
+
+	def restart():
+		pass
 		
